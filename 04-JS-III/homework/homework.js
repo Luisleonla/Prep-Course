bc78d4d46d7449f9cf3ea21ebe8b3faa0ae9a574
Luisleonla <luis.ant.arti.l@gmail.com>
2022-07-20 13:44:56 -0500

    modification 220720

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -142,12 +142,31 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  multi = 1
+  argumentfin = ''
+  for(i = 0; arguments[i] !== undefined; i++){
+      multi = multi * arguments[i]
+      argumentfin = i
+  }
+  if(argumentfin === ''){
+    return 0
+  }else return multi
+  
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  cuenta = 0
+  for(i = 0; i < arreglo.length; i++){
+    if(arreglo[i]> 18){
+      cuenta = cuenta + 1
+    }
+  }
+  if(i = arreglo.length){
+    return cuenta
+  }
 
 }
 
@@ -157,7 +176,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia === 1 || numeroDeDia === 7){
+    return 'Es fin de semana'
+  }else return 'Es día laboral'
 } 
 
 
@@ -165,6 +186,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var nString = n.toString();
+  var esNueve = nString[0];
+  if(esNueve = 9){
+    return true
+  }else return false
   
 }
 
